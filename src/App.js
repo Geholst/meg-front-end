382dc7568162324bc68251209dc99a6fe7057ca6
Geholst <geholst@msn.com>
2023-06-05 13:51:01 -0700

    added sign up and logins

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Nav from './Nav';
 import Home from './components/Home';
 import Login from './components/Login';
+import Signup from './components/Signup'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   </main>   
    </BrowserRouter>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,8 @@
-
-export default function Login (){
+export default function Signup (){
     return (
         <section>
-            <h1>Login page!</h1>
-  <span class="input-group-text">@</span>
+            <h1>Signup page!</h1>
+            <span class="input-group-text">@</span>
   <input type="text" class="form-control" placeholder="Username" id="username-input" aria-label="Username" aria-describedby="basic-addon1" />
   <span class="input-group-text">#</span>
   <input type="password" class="form-control"  placeholder="Password"  id="password-input" aria-label="Password" aria-describedby="basic-addon1" />
@@ -11,5 +10,5 @@ export default function Login (){
     <button id="login-btn" type="submit" class="btn btn-success">Submit</button>
 
         </section>
-    );
+    )
 }
