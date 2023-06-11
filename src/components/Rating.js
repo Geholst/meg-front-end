1954afb44e37ff7072136cb48a068d571a01dfbd
Joel Perez <jxp489@gmail.com>
2023-06-11 14:48:06 -0700

    Modified, Journey, Experience, ER Contacts, Rating, fixed fetch requestson most and other dashboard to backend related items

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-
+import { Modal, Button, Form } from "react-bootstrap";
+let userId;
+userId = localStorage.getItem("userId");
 const Rating = () => {
   const [ratings, setRatings] = useState([]);
   const [newRating, setNewRating] = useState({
-    userId: localStorage.getItem("userId"),
+    userId: userId,
     journeyId: "",
     rating: "",
     comments: "",
@@ -20,22 +22,19 @@ const Rating = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch(
-        "https://meg-backend.herokuapp.com/api/rating",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(newRating),
-        }
-      );
+      const response = await fetch("https://meg-backend.herokuapp.com/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newRating),
+      });
 
       if (response.ok) {
         const data = await response.json();
         setRatings([...ratings, data]);
         setNewRating({
-          userId: localStorage.getItem("userId"),
+          userId: userId,
           journeyId: "",
           rating: "",
           comments: "",
@@ -48,49 +47,58 @@ const Rating = () => {
     }
   };
 
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <div>
       <h2>Ratings</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="journeyId">Journey ID:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="journeyId"
-            name="journeyId"
-            value={newRating.journeyId}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="rating">Rating:</label>
-          <input
-            type="number"
-            className="form-control"
-            id="rating"
-            name="rating"
-            min="1"
-            max="5"
-            value={newRating.rating}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="comments">Comments:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="comments"
-            name="comments"
-            value={newRating.comments}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Add Rating
-        </button>
-      </form>
+      <Button onClick={handleOpenModal}>Add Rating</Button>
+
+      <Modal show={modalOpen} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Rating</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formRating">
+              <Form.Label>Rating:</Form.Label>
+              <Form.Control
+                type="number"
+                name="rating"
+                min="1"
+                max="5"
+                value={newRating.rating}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="formComments">
+              <Form.Label>Comments:</Form.Label>
+              <Form.Control
+                type="text"
+                name="comments"
+                value={newRating.comments}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Button className="mt-2" variant="primary" type="submit">
+              Save Rating
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
       {ratings.length === 0 ? (
         <p>No ratings found.</p>
